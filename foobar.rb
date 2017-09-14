64f3844273159dfab0dd6b9dd397035be4b261d4
Wilbur Shi <wilbu@DESKTOP-SBHQII6.(none)>
2017-09-13 19:16:13 -0700

    Complete hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,15 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    # can find unique elements before doing other operations,
+    # and then remove duplicates and odd numbers in one go
+
+    a.map! { |x| x.to_i + 2 }
+    a.uniq!
+    a.reject! { |x| x%2!=0 or x>10 }
+    # base case is 0, otherwise add to the sum variable by x each time
+    a.inject(0) {|sum, x| sum+x}
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a+b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  # first sort array, then map an increment by 1 function to each element
+  a.sort.map! {|x| x+1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + ' ' + last_name
 end
 
 def blockin_time(a)
